fb4498d5b0be0179fe12fafa5debc9ba4e5afae5
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-06 00:37:38 +0100

    Create createMainContent function

---
+++ src/components/monumentCard/index.ts
@@ -0,0 +1,6 @@
+export const createMainContent = (): HTMLElement => {
+  const mainContent = document.createElement("main");
+  mainContent.classList.add("main-content");
+
+  return mainContent;
+};
