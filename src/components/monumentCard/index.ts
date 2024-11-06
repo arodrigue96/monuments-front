734cd47eaa823669471692a2b91b698d00eb0c27
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-06 23:14:24 +0100

    Create monumentCardsList component

--- src/components/monumentCard/index.ts
+++
@@ -1,6 +0,0 @@
-export const createMainContent = (): HTMLElement => {
-  const mainContent = document.createElement("main");
-  mainContent.classList.add("main-content");
-
-  return mainContent;
-};
