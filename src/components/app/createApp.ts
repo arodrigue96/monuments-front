f2e2db5eaa5b43a27ad14f61a526332b2008cf87
Aniol Rodriguez <arodrigue96@gmail.com>
2024-11-06 23:06:10 +0100

    Append mainHeader and monumentCardsList to app container

--- src/components/app/createApp.ts
+++ src/components/app/createApp.ts
@@ -1,12 +1,16 @@
-import { createMainContent } from "../monumentCard";
-import "./createApp.css";
+import { createMonumentCardsList } from "../monumentCardsList/createMonumentCardsList";
+import createHeader from "../header/createHeader";
+import "./app.css";
 
 const createApp = (): HTMLElement => {
   const appContainer = document.createElement("div");
   appContainer.classList.add("main-container");
 
-  const mainContent = createMainContent();
-  appContainer.appendChild(mainContent);
+  const mainHeader = createHeader();
+  const monumentCardsList = createMonumentCardsList();
+
+  appContainer.appendChild(mainHeader);
+  appContainer.appendChild(monumentCardsList);
 
   return appContainer;
 };
