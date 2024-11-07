f64dd47e5de8efdb4d7c995c810063026d109415
Erik Riquelme <erikone2311@gmail.com>
2024-11-08 00:27:23 +0100

    Add menu selection test for header

--- src/components/header/header.test.ts
+++ src/components/header/header.test.ts
@@ -1,9 +1,13 @@
 import createHeader from "./createHeader";
 
+const screen = document.createElement("div");
+afterEach(() => {
+  screen.innerHTML = "";
+});
+
 describe("Given the header component", () => {
   describe("When rendered", () => {
     test("Then it should show 'Monuments' inside a heading", () => {
-      const screen = document.createElement("div");
       const expectedTitle = /Monuments/i;
 
       const header = createHeader();
@@ -12,6 +16,26 @@ describe("Given the header component", () => {
 
       expect(title).not.toBeNull();
       expect(title?.textContent).toMatch(expectedTitle);
+    });
+
+    test("Then it should show a menu selection with 'Monuments' and 'Add Monument' inside a anchor each", () => {
+      const expectedAddMonumentAnchorText = /Add Monument/i;
+      const expectedMonumentsAnchorText = /Monuments/i;
+
+      const header = createHeader();
+      screen.appendChild(header);
+
+      const menuSelectionContent = screen.querySelectorAll(
+        ".selection-menu__anchor-container",
+      );
+
+      const monumentsAnchor = menuSelectionContent[0];
+      const addMonumentAnchor = menuSelectionContent[1];
+
+      expect(monumentsAnchor.textContent).toMatch(expectedMonumentsAnchorText);
+      expect(addMonumentAnchor.textContent).toMatch(
+        expectedAddMonumentAnchorText,
+      );
     });
   });
 });
