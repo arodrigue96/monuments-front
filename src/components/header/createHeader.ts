7931d6be73258db1523c831d4a3adb57e5406374
Erik Riquelme <erikone2311@gmail.com>
2024-11-07 08:59:17 +0100

    Create page selection menu structure

--- src/components/header/createHeader.ts
+++ src/components/header/createHeader.ts
@@ -4,7 +4,17 @@ const createHeader = (): HTMLElement => {
   const mainHeader = document.createElement("header");
   mainHeader.classList.add("main-header");
 
-  mainHeader.innerHTML = "<h1>Monuments</h1>";
+  mainHeader.innerHTML = `
+  <h1>Monuments</h1>
+  <ul class='selection-menu'>
+    <li class='selection-menu__anchor-container'>
+      <a class='selection-menu__link' href=''><span>Monuments</span></a>
+    </li>
+    <li class='selection-menu__anchor-container'>
+      <a class='selection-menu__link' href=''><span>Add Monument</span></a>
+    </li>
+  </ul>
+  `;
 
   return mainHeader;
 };
