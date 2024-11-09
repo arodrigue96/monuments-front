4761aa96bae2eaa68a3cea132ef21a86d552c8a6
Erik Riquelme <erikone2311@gmail.com>
2024-11-09 23:29:04 +0100

    Add environment url in getMonument function

--- src/monument/getMonuments.ts
+++ src/monument/getMonuments.ts
@@ -1,12 +1,13 @@
 import type Monument from "./Monument";
 
+const url =
+  (import.meta.env.VITE_URL as string) ??
+  "https://monuments-back-ix7j.onrender.com/monuments";
+
 const getMonuments = async (): Promise<Monument[]> => {
-  const response = await fetch(
-    "https://monuments-back-ix7j.onrender.com/monuments",
-    {
-      method: "GET",
-    },
-  );
+  const response = await fetch(url, {
+    method: "GET",
+  });
 
   const monuments = (await response.json()) as Monument[];
 
