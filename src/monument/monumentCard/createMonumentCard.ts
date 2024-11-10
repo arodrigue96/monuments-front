1d28c239b53836667fdd82029639ab080ea014cf
Erik Riquelme <erikone2311@gmail.com>
2024-11-10 17:44:19 +0100

    Add monuments card in monumentCardsList

--- src/monument/monumentCard/createMonumentCard.ts
+++ src/monument/monumentCard/createMonumentCard.ts
@@ -1,12 +1,14 @@
 import { type Monument } from "../types";
 import "./monumentCard.css";
 
-const createMonumentCard = ({ name }: Monument): HTMLElement => {
-  const cardContainer = document.createElement("article");
+const createMonumentCard = ({ name }: Monument): HTMLLIElement => {
+  const cardContainer = document.createElement("li");
   cardContainer.classList.add("monument-cards-lists__card");
 
   cardContainer.innerHTML = `
-  <h2 class='card__title'>${name}</h2>
+  <article>
+    <h2 class='card__title'>${name}</h2>
+  </article>
   `;
 
   return cardContainer;
