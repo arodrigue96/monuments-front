4fd1ba68b73cd263e2ed9917cd4f0ca7a164ece4
Erik Riquelme <erikone2311@gmail.com>
2024-11-10 22:40:50 +0100

    Add image and information to monument card component

--- src/monument/monumentCard/createMonumentCard.ts
+++ src/monument/monumentCard/createMonumentCard.ts
@@ -1,13 +1,26 @@
 import { type Monument } from "../types";
 import "./monumentCard.css";
 
-const createMonumentCard = ({ name }: Monument): HTMLLIElement => {
+const createMonumentCard = ({
+  name,
+  description,
+  imageUrl,
+  country,
+  city,
+}: Monument): HTMLLIElement => {
   const cardContainer = document.createElement("li");
-  cardContainer.classList.add("monument-cards-lists__card");
 
   cardContainer.innerHTML = `
-  <article>
-    <h2 class='card__title'>${name}</h2>
+  <article class='monument-cards-lists__card'>
+    <img class='card__image' src='${imageUrl}' alt='Monument: ${name}'  width='340' height='250'>
+    <div class='card__monument-information'>
+      <h2 class='card__title'>${name}</h2>
+      <span class='card__description'>${description}</span>
+      <div class='card__location'>
+        <span>Country: ${country}</span> 
+        <span>City: ${city}</span>
+      </div>
+    </div>
   </article>
   `;
 
